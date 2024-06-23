7cdf6c3e0ab74a1c51113e7b5d7d9afe05a12cbd
yokohama <yuhei.yokohama@gmail.com>
2024-06-23 16:28:17 +0900

    本番環境の環境変数デバッグ

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,5 @@
-use tracing::info;
+use std::env;
+use tracing::{debug, info};
 
 mod config;
 mod routes;
@@ -12,10 +13,28 @@ async fn main() {
     config::logging::app_log_tracing();
 
     info!("#### start application ####");
+    print_env();
+
     axum::serve(
         tokio::net::TcpListener::bind("0.0.0.0:3000")
             .await
             .unwrap(),
         routes::get()
     ).await.unwrap();
+}
+
+fn print_env() {
+    let env_keys = vec![
+        "RUST_BACKTRACE",
+        "RUST_LOG",
+        "DATABASE_URL",
+        "JWT_SECRET",
+    ];
+
+    for key in env_keys {
+        match env::var(key) {
+            Ok(value) => debug!("{}: {}", key, value),
+            Err(_) => debug!("None! {}", key),
+        }
+    }
 }
