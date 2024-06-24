e6b9d46298fc8c2ae97e37f23988c13f1a129410
yokohama <yuhei.yokohama@gmail.com>
2024-06-24 13:01:14 +0900

    entrypoint.shデバッグ

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 echo "#### entrypoint.sh start #####"
+
+echo "Current directory: $(pwd)"
+echo "Listing current directory contents:"
+ls -l
+
 diesel migration run
+
 echo "#### entrypoint.sh end   #####"
 
 exec "$@"
