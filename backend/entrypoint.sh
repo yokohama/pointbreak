cf273a32b719298f77b5dc29f48c1d0d2c36adfa
yokohama <yuhei.yokohama@gmail.com>
2024-06-24 14:45:33 +0900

    entrypoint.shデバッグ

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,23 +2,7 @@
 
 echo "#### entrypoint.sh start #####"
 
-echo "Current directory: $(pwd)"
-echo "Listing current directory contents:"
-
-echo "#### /app                #####"
-ls -l
-
-echo "#### /app/migrations     #####"
-ls -l migrations/
-
-echo "#### /app/src/schema.rs  #####"
-ls -l src/schema.rs
-
-echo "#### which diesel        #####"
-which diesel
-
-cd /app
-
+#cd /app
 diesel migration run
 
 echo "#### entrypoint.sh end   #####"
