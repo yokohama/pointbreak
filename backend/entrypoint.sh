28a28de8f674908f88e5a48ce1a38c27684e9daf
yokohama <yuhei.yokohama@gmail.com>
2024-08-12 14:17:23 +0900

    deploy test

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
 
+echo "##########"
+echo "##########"
+echo "##########"
+echo "##########"
+echo "##########"
+echo "##########"
+echo "##########"
+echo $DATABASE_URL
 sqlx migrate run
+if [ $? -ne 0 ]; then
+	echo "***********"
+else
+	echo "+++++++++++"
+fi
+echo "##########"
+echo "##########"
+echo "##########"
+echo "##########"
+echo "##########"
+echo "##########"
+echo "##########"
+echo "##########"
+echo "##########"
 
 exec "$@"
